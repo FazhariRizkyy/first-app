20d91b2fa9f821e212849492942e7dc32cb926d6
FazhariRizkyy <fazharirizkyy@gmail.com>
2025-02-05 23:19:52 +0700

    Belajar Tambah Image

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,25 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:my_first_project/styled_text.dart'; 
+import 'package:my_first_project/styled_text.dart';
 
 var startAlignment = Alignment.topLeft;
 var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
+
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Color.fromARGB(255, 26, 2, 80),
-          Color.fromARGB(255, 25, 7, 98)
-        ], begin: startAlignment, 
-        end: endAlignment
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText()
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/azril-andre.png', 
+              width: 300
+              ),
+            SizedBox(height: 20),
+            Text(
+              'ADUH GANTENGNYA',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+
+//   final List<Color> colors;
+
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors, 
+//         begin: startAlignment, 
+//         end: endAlignment
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyledText('ESOK KOLOH TAK CIKGU')
+//       ),
+//     );
+//   }
+// }
